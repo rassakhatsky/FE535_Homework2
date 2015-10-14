b2ce4c3a0e4d3ff6dd940c7dac371409dfd4db6e
rassakhatsky <rassakhatsky@gmail.com>
2015-10-14 15:41:00 -0400

    Initial commit

--- BlackFormula.py
+++ BlackFormula.py
@@ -50,15 +50,3 @@ class Option():
             self.vol -= (option.calculatePrice() - price) / self.vega()
             self.factors()
         return float(round(self.vol * 100, 4))
-
-# put = Option(option='put', spotPrice=1314.49, strikePrice=1314.49, timeDays=180, rate=0.78525 / 100.0,
-#              dividendYeld=2.211 / 100.0, volatility=20.252 / 100.0)
-# put.calculatePrice()
-#
-# call = Option(option='call', spotPrice=1314.25, strikePrice=1314.25, timeDays=30, rate=0.261 / 100.0,
-#              dividendYeld=2.886 / 100.0, volatility=16.252 / 100.0)
-# call.calculatePrice()
-#
-# vol  = Option(option='call', spotPrice=1314.25, strikePrice=1314.25, timeDays=30, rate=0.261 / 100.0,
-#              dividendYeld=2.886 / 100.0, volatility=200 / 100.0)
-# vol.imp_vol(23.47)
--- application.py
+++ application.py
@@ -1,13 +1,108 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template_string, request, flash
 from wtforms import RadioField, FloatField, IntegerField
 from wtforms.validators import DataRequired
 from flask.ext.wtf import Form
+
 from  BlackFormula import Option
 
 application = Flask(__name__)
 
 SECRET_KEY = 'blablbablablbalablbasecretkeyissosecret'
 
+template = '''
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>FE535 - Homework 2</title>
+    <style type="text/css">
+        .bs-example {
+            margin: 20px;
+        }
+    </style>
+
+    {% if title %}
+        <title>{{ title }} - FE535</title>
+    {% else %}
+    {% endif %}
+    {% with messages = get_flashed_messages() %}
+        {% if messages %}
+            <ul class=flashes>
+                {% for message in messages %}
+                    <li>{{ message }}</li>
+                {% endfor %}
+            </ul>
+        {% endif %}
+    {% endwith %}
+    {% block body %}{% endblock %}
+
+</head>
+<body>
+<div>FE535: <a href="/">Homework 2</a> |
+    <a href="/blackformula">Black Formula calculator (example 1)</a> |
+    <a href="/volatility">Implied volatility calculator (example 2)</a> |
+</div>
+<hr>
+<h1>Implied volatility calculator</h1>
+
+<form action="" method="post" name="calculator">
+    {{ form.hidden_tag() }}
+    <p>
+        Days in Year:
+        {% for subfield in form.daysYear %}
+            <tr>
+                <td>{{ subfield }}</td>
+                <td>{{ subfield.label }}</td>
+            </tr>
+        {% endfor %}
+    </p>
+
+    <p>
+        Option type:
+        {% for subfield in form.optionType %}
+            <tr>
+                <td>{{ subfield }}</td>
+                <td>{{ subfield.label }}</td>
+            </tr>
+        {% endfor %}
+    </p>
+    <p>{{ form.spotPrice.label }}: {{ form.spotPrice(size=10) }}</p>
+
+    <p>{{ form.strikePrice.label }}: {{ form.strikePrice(size=10) }}</p>
+
+    <p>{{ form.timeDays.label }}: {{ form.timeDays(size=10) }}</p>
+
+    <p>{{ form.rate.label }}: {{ form.rate(size=10) }}</p>
+
+    <p>{{ form.dividendYeld.label }}: {{ form.dividendYeld(size=10) }}</p>
+    <br><br>
+
+    <p>
+        Target Action:
+        {% for subfield in form.target %}
+            <br>
+            <tr>
+                <td>{{ subfield }}</td>
+                <td>{{ subfield.label }}</td>
+            </tr>
+        {% endfor %}
+
+    </p>
+    <p>{{ form.volatility.label }}: {{ form.volatility(size=10) }}</p>
+
+    <p>{{ form.price.label }}: {{ form.price(size=10) }}</p>
+
+    <p><input type="submit" value="Calculate"></p>
+</form>
+</body>
+</html>
+<head>
+</head>
+
+<script>
+    function clear() {
+        document.getElementById("myForm").reset();
+    }
+</script>'''
 
 
 class Calculator(Form):
@@ -29,8 +124,6 @@ class Calculator(Form):
     volatility = FloatField('Volatility, %')
     price = FloatField('Option Price, $')
 
-def hi():
-    return 'Hi!'
 
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/volatility', methods=['GET', 'POST'])
@@ -134,7 +227,7 @@ def calculator():
             form.volatility.raw_data = [str(volatility)]
         else:
             flash('Volatility or Option Price has to be set')
-    return render_template('calculator.html', title='FE535 - Homework 2', form=form)
+    return render_template_string(template, title='FE535 - Homework 2', form=form)
 
 
 if __name__ == '__main__':
